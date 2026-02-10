d7efb710c1d10345afa2665e9510ca9dd0ac4a16
Manasshivam <jimanas22@gmail.com>
2026-02-10 23:57:34 +0530

    Refactor div for tech brand banner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -59,9 +59,8 @@ const App = () => {
     <CartProvider>
       <WishlistProvider>
         <Router>
-         <div className="bg-black text-white text-center py-2 text-xs md:text-sm font-medium tracking-widest uppercase">
-         🇮🇳 India's First Women's Tech Brand
-         </div>
+          <div className="bg-black text-white text-center py-2 text-xs md:text-sm font-medium tracking-widest uppercase relative z-[1000]">🇮🇳 India's First Women's Tech Brand
+          </div>
           {/* ================= NAVBAR (GLOBAL) ================= */}
           <Navbar2 />
 
